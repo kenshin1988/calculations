a6150d24c39a53b7d7d5fcabebeb06567b7e6592
Tushar Hindlekar <tusharhindlekar3@gmail.com>
2022-06-12 13:59:12 +0530

    first commit

---
+++ inc/calc.h
@@ -0,0 +1,5 @@
+
+int32_t add(int32_t x, int32_t y);
+int32_t sub(int32_t x, int32_t y);
+int32_t mul(int32_t x, int32_t y);
+int32_t div(int32_t x, int32_t y);
---
+++ src/calc.c
@@ -0,0 +1,22 @@
+
+
+int32_t add(int32_t x, int32_t y)
+{
+    return x+y;
+}
+
+int32_t sub(int32_t x, int32_t y)
+{
+    return x-y;
+}
+
+int32_t mul(int32_t x, int32_t y)
+{
+    return x*y;
+}
+
+int32_t div(int32_t x, int32_t y)
+{
+    return x/y;
+}
+
